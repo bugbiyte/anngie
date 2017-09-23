f73616fefc8e1d39445a0f3443c44886218eb64e
bugbiyte <thakillerbee@gmail.com>
2017-09-23 09:33:00 -0700

    final project (still in progress tho)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-// Make it rain!!
-$('#showModal').on('click', function (){
-  console.log ('buton is clicked')
-  $('.modal-background').fadeIn();
+// // Make it rain!!
+// $('#showModal').on('click', function (){
+//   console.log ('buton is clicked')
+//   $('.modal-background').fadeIn();
+// })
+//
+// $('#close').on('click', function(){
+//  console.log ('buton is clicked')
+//    $('.modal-background').fadeOut();
+// })
+
+// When the user clicks the hamburger icon (.hamburger)
+  // Slide toggle the menu (nav ul)
+
+$('.hamburger').on('click', function (){
+  console.log('button is clicked')
 })
 
-$('#close').on('click', function(){
- console.log ('buton is clicked')
-   $('.modal-background').fadeOut();
+$('.nav-links').addClass('active');
 })
+
+$('.close').on ('click', function(){
+  console.log('button is clicked')
+$('.nav-links').removeClass('active');
+});
